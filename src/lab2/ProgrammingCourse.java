07d11af416633796e75dfa2b106bcdafe472c2f1
Adam <Adam@Adam-LAPTOP>
2015-02-09 18:04:13 -0600

    created interface for lab2

---
+++ src/lab2/ProgrammingCourse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Adam
+ */
+public interface ProgrammingCourse {
+    public void createProgrammingCourse(String courseName, String courseNumber);
+
+    public String getCourseNumber();
+
+    public void setCourseNumber(String courseNumber);
+
+    public double getCredits();
+    
+    public void setCredits(double credits);
+
+    public String getCourseName();
+
+    public void setCourseName(String courseName);
+  
+}
